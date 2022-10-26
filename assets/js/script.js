088feb75774fee5472f04d3adcb3f3401f3405bf
zorobijan <82671021+zorobijan@users.noreply.github.com>
2022-10-26 11:23:08 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,6 @@ var saveButton = $('.saveBtn col-md-1');
 let today = moment();
 $("#time-display").text(today.format("MMM Do, YYYY"));
 
-var printText = function (text) {
-    var textAreaEl = $('<textarea>');
-    var listDetail = text.concat(' on ');
-    textAreaEl.addClass('col-md-10 description').text(listDetail);
-    textAreaEl.appendTo(skillsListEl);
-  };
-
 var saveNote = function (event) {
   event.preventDefault();
   var note = document.getElementByClass("col-md-10 description").value;
