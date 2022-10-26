995418ade4a38d368fa4ecb8b73d58b8a8a1bf64
Matthew Bascom <matt.bijan@gmail.com>
2022-10-25 21:03:38 -0500

    created textarea ids and associated variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,16 @@
 //     //loads DOM before page loads//
 
 //   })
+var text1hr = $('#text1hr');
+var text2hr = $('#text2hr');
+var text3hr = $('#text3hr');
+var text4hr = $('#text4hr');
+var text5hr = $('#text5hr');
+var text6hr = $('#text6hr');
+var text7hr = $('#text7hr');
+var text8hr = $('#text8hr');
+var text9hr = $('#text9hr');
+
 let today = moment();
 $("#time-display").text(today.format("MMM Do, YYYY"));
 
