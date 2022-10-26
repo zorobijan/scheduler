5a6ef15991ba1260d0cc259b9da0e6b7af8003d6
Matthew Bascom <matt.bijan@gmail.com>
2022-10-26 11:21:21 -0500

    created save and load functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
-// $(document).ready(function() {
-//     //loads DOM before page loads//
+$(document).ready(function() {
+    //loads DOM before page loads//
 
-//   })
-var text1hr = $('#text1hr');
-var text2hr = $('#text2hr');
-var text3hr = $('#text3hr');
-var text4hr = $('#text4hr');
-var text5hr = $('#text5hr');
-var text6hr = $('#text6hr');
-var text7hr = $('#text7hr');
-var text8hr = $('#text8hr');
-var text9hr = $('#text9hr');
+  })
+var saveButton = $('.saveBtn col-md-1');
 
 let today = moment();
 $("#time-display").text(today.format("MMM Do, YYYY"));
 
+var printText = function (text) {
+    var textAreaEl = $('<textarea>');
+    var listDetail = text.concat(' on ');
+    textAreaEl.addClass('col-md-10 description').text(listDetail);
+    textAreaEl.appendTo(skillsListEl);
+  };
+
+var saveNote = function (event) {
+  event.preventDefault();
+  var note = document.getElementByClass("col-md-10 description").value;
+  localStorage.setItem("note", note);
+}
+var loadNote = function () {
+  var note = localStorage.getItem("col-md-10 description");
+  document.getElementByClass("note").value = note;
+}
+
+loadNote();
+saveButton.on('click', saveNote);
