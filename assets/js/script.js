80ca20c9a70fdcfb36d01cb244295389dfca6bc8
Matthew Bascom <matt.bijan@gmail.com>
2022-11-01 22:13:50 -0500

    added load function for local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,55 @@
-// your getElementByClass is not a method and should instead be querySelector's or querySelectorAll if you're trying to target multiple elements that all have that class name
-// however dane and I recommend using event.target otherwise referred to as -event delegation- to listen on a container for whatever you have in your event listener
-// I've provided some example code that you will need to modify to actually do what you want below
 
 $(document).ready(function() {
   //loads DOM before page loads//
 
 })
 
-let containerEl = document.querySelector(".container")
+const containerEl = document.querySelector(".container")
+const textAreaEl = document.querySelector(".description");
+const buttonEl = document.getElementById(".saveBtn");
 
 let today = moment();
 $("#time-display").text(today.format("MMM Do, YYYY"));
 
 containerEl.addEventListener("click", function(event){
   if(event.target.matches("button")){
-    let input_textarea = event.target.previousElementSibling.value
-    localStorage.setItem('description', input_textarea);
-  console.log(event.target.previousElementSibling.value)
+    let input_textareaOne = event.target.previousElementSibling.value
+    let input_textareaTwo = event.target.previousElementSibling.value
+    let input_textareaThree = event.target.previousElementSibling.value
+    let input_textareaFour = event.target.previousElementSibling.value
+    let input_textareaFive = event.target.previousElementSibling.value
+    let input_textareaSix = event.target.previousElementSibling.value
+    let input_textareaSeven = event.target.previousElementSibling.value
+    let input_textareaEight = event.target.previousElementSibling.value
+    let input_textareaNine = event.target.previousElementSibling.value
+    localStorage.setItem('1hr', input_textareaOne);
+    localStorage.setItem('2hr', input_textareaTwo);  
+    localStorage.setItem('3hr', input_textareaThree);
+    localStorage.setItem('4hr', input_textareaFour);
+    localStorage.setItem('5hr', input_textareaFive);
+    localStorage.setItem('6hr', input_textareaSix);
+    localStorage.setItem('7hr', input_textareaSeven);
+    localStorage.setItem('8hr', input_textareaEight);
+    localStorage.setItem('9hr', input_textareaNine);
+
+    console.log(localStorage.getItem('1hr'))
+    console.log(localStorage.getItem('2hr'))
+    console.log(localStorage.getItem('3hr'))
+
   }
-  // else if (event.target.matches("i")){
-  // console.log(event.target.parent.previousElementSibling.value)
-  // }
-  });
+});
+  
+  function loadEvents() {
+    let hourOne = localStorage.getItem("#hourOne");
+    let hourTwo = localStorage.getItem("#hourTwo");
+    let hourThree = localStorage.getItem("#hourThree");
+    let hourFour = localStorage.getItem("#hourFour");
+    let hourFive = localStorage.getItem("#hourFive");
+    let hourSix = localStorage.getItem("#hourSix");
+    let hourSeven = localStorage.getItem("#hourSeven");
+    let hourEight = localStorage.getItem("#hourEight");
+    let hourNine = localStorage.getItem("#hourNine");
+  
 
-  // function SaveBtn(){
-  //   let input_textarea = document.querySelector('.description');
-  //   let save_button = document.querySelector('.saveBtn');
-    
-  //   save_button.addEventListener('click', updateOutput);
-    
-  //   input_textarea.textContent = localStorage.getItem('.description');
-  //   input_textarea.value = localStorage.getItem('.description');
-    
-  //   function updateOutput() {
-  //        Del();
-  //     localStorage.setItem('content', input_textarea.value);
-  //       input_textarea.textContent = input_textarea.value;
-  //   }
-  //   }
-// var note = document.querySelector(".description").value;
-// notice how this note variable is only going to console.log the first textarea's value because querySelector just looks for the first instance of a element
-//with the class description
-// console.log(note)
-
-
-// loadNote();
-// saveButton.on('click', saveNote);
-
-// this type of code will allow you to listen for button clicks that occur inside of your element with a class of container here I'm using an anonymous function with (event) passed into it
-// when you do this you can look for the text areas relative to the button that was pressed. 
+  }
+  loadEvents();
